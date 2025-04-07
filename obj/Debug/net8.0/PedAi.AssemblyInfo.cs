d3d7a4651eec712ba79e10398a48816814d8bebc
Diego Borba <diegoborbas03@gmail.com>
2025-04-07 17:59:47 -0300

    Teste Pedido com mais de 1 item

--- obj/Debug/net8.0/PedAi.AssemblyInfo.cs
+++ obj/Debug/net8.0/PedAi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PedAi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0adaabcd89e44d6b63ebcf949d7258ff1bf57bc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94cd15bea2b18ad44ed046a9f694ea8c7fd1c5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PedAi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PedAi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
